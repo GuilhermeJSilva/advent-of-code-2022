4d414fb3ddf0d4fffa46ca1c86fe6fede8d7bb9e
Guilherme Silva <guilhermejosesilva@gmail.com>
2022-12-03 12:07:23 +0000

    Solution 2022 day 3

--- runner/src/filecache.rs
+++ runner/src/filecache.rs
@@ -1,4 +1,3 @@
-use base16ct;
 use core::panic;
 use sha2::{Digest, Sha256};
 use std::fs;
--- runner/src/solutions/y2022/day03.rs
+++ runner/src/solutions/y2022/day03.rs
@@ -1,5 +1,9 @@
 use super::super::AocSolution;
-use std::sync;
+use itertools::Itertools;
+use std::{
+    collections::{HashMap, HashSet},
+    sync,
+};
 
 pub struct Day03;
 
@@ -9,12 +13,112 @@ impl Day03 {
     }
 }
 
-impl AocSolution for Day03 {
-    fn solve_part1(&self, _input: String) -> String {
-        todo!()
+struct Compartment {
+    item_count: HashMap<char, usize>,
+}
+
+impl From<&str> for Compartment {
+    fn from(input: &str) -> Self {
+        Compartment {
+            item_count: input.chars().fold(HashMap::new(), |mut map, c| {
+                *map.entry(c).or_insert(0) += 1;
+                map
+            }),
+        }
+    }
+}
+
+impl Compartment {
+    fn keys(&self) -> HashSet<char> {
+        HashMap::keys(&self.item_count).copied().collect()
+    }
+}
+
+pub struct Rucksack {
+    components: [Compartment; 2],
+}
+
+impl From<&str> for Rucksack {
+    fn from(input: &str) -> Self {
+        Rucksack {
+            components: [
+                Compartment::from(&input[..input.len() / 2]),
+                Compartment::from(&input[input.len() / 2..]),
+            ],
+        }
+    }
+}
+
+impl Rucksack {
+    fn unique_key(self) -> Option<char> {
+        self.components[0]
+            .keys()
+            .intersection(&self.components[1].keys())
+            .next()
+            .copied()
     }
 
-    fn solve_part2(&self, _input: String) -> String {
-        todo!()
+    fn keys(self) -> HashSet<char> {
+        self.components
+            .iter()
+            .map(Compartment::keys)
+            .fold(HashSet::new(), |mut set, comp| {
+                set.extend(comp.iter());
+                set
+            })
+    }
+}
+
+fn priority(c: char) -> u32 {
+    if c.is_lowercase() {
+        c as u32 - 'a' as u32 + 1
+    } else {
+        c as u32 - 'A' as u32 + 27
+    }
+}
+
+impl AocSolution for Day03 {
+    fn solve_part1(&self, input: String) -> String {
+        let res: u32 = input
+            .lines()
+            .map(Rucksack::from)
+            .flat_map(Rucksack::unique_key)
+            .map(priority)
+            .sum();
+
+        return format!("{}", res);
+    }
+
+    fn solve_part2(&self, input: String) -> String {
+        let res: u32 = input
+            .lines()
+            .map(Rucksack::from)
+            .map(Rucksack::keys)
+            .batching(|it| {
+                it.take(3)
+                    .reduce(|a, b| a.intersection(&b).copied().collect())
+            })
+            .map(|collection| collection.into_iter().next().unwrap())
+            .map(priority)
+            .sum();
+        return format!("{}", res);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::y2022::day03::Day03;
+    use crate::solutions::AocSolution;
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+    #[test]
+    fn part1() {
+        let solver = Day03 {};
+        assert_eq!(solver.solve_part1(INPUT.into()), "157");
+    }
+
+    #[test]
+    fn part2() {
+        let solver = Day03 {};
+        assert_eq!(solver.solve_part2(INPUT.into()), "70");
     }
 }
